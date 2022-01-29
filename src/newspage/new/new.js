7eaadf4c2e3777e356fc8c7e53b27cb8648cd533
Mauricio <moromanya@hotmail.com>
2022-01-28 21:05:50 -0300

    Create news page

--- src/newspage/new/new.js
+++ src/newspage/new/new.js
@@ -1,15 +1,27 @@
 import './new.css'
+import { text } from './data'
+
 
 const New = () => {
     return <>
         <div className='New-page'>
             <div className='new-page-box-title'>
                 <h1 className='new-page-title'> Pfizer gets EU regulator's nod for oral Covid drug</h1>
+                <h5>19.8.2021</h5>
             </div>
+            <h3 className='new-page-subtitle'>European Medicines Agency recommends use of at-home treatment Paxlovid</h3>
             <div className='new-page-line'></div>
             <div className='new-page-img-box'>
                 <img className='new-page-img' src='./images/home/down-home/pfizer.jpg' />
+                <p className='new-page-img-footer'>This handout photo provided by Pfizer, shows the making of its experimental Covid-19 antiviral pill Paxlovid at a laboratory in Freiburg, Germany in November.
+                    Photo credit: AFP</p>
             </div>
+            {text.map(paragraph => {
+                return <>
+                    <p className='new-page-text'>{paragraph}</p>
+                </>
+            })}
+
         </div>
     </>
 }
--- src/newspage/rightbanner/rightban.js
+++ src/newspage/rightbanner/rightban.js
@@ -1,7 +1,28 @@
+import './rightbanner.css'
+import { data } from './data.js'
+
 const RightBanner = () => {
     return <>
-        <div clasName='news-right-banner'>
-
+        <div className='news-right-banner-box'>
+            <div className='right-banner-top'>
+                <img className='right-banner-top-img' src='./images/home/china-demands.jpg' />
+                <h3 className='right-banner-top-title'>China demands US halt Olympics 'interference'</h3>
+                <div className='right-banner-top-tags'>
+                    <h6 className='right-banner-top-list'>19.10.2021</h6>
+                    <h6 className='right-banner-top-list'>Bussines</h6>
+                </div>
+            </div>
+            <div className='right-banner-bot'>
+                {data.map((notice, i) => {
+                    const { title, img } = notice
+                    return <>
+                        <div className='right-banner-bot-notice'>
+                            <img className='right-banner-bot-img' src={img} />
+                            <h4 className='right-banner-bot-title'>{title}</h4>
+                        </div>
+                    </>
+                })}
+            </div>
         </div>
     </>
 }
